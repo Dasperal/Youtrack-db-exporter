9b3c831b75d8caf0b74f2c26ce48ce2c4485202d
Dasperal <dasperal1@gmail.com>
2024-12-21 22:15:12 +0300

    Setup gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    java
+    kotlin("jvm") version "2.1.0"
+    application
+    idea
+}
+
+group = "io.github.dasperal"
+version = "0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.xodus:xodus-openAPI:2.0.1")
+    implementation("org.jetbrains.xodus:xodus-entity-store:2.0.1")
+    implementation("org.jetbrains.xodus:xodus-environment:2.0.1")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+    }
+}
+
+application {
+    mainClass = "io.github.dasperal.Main"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "io.github.dasperal.Main"
+    }
+}
