e0de58a562b318d765a00b380a58ad15721790d2
Dasperal <dasperal1@gmail.com>
2024-12-31 00:53:39 +0300

    Export issue links to csv

--- src/main/kotlin/io/github/dasperal/Main.kt
+++ src/main/kotlin/io/github/dasperal/Main.kt
@@ -14,22 +14,31 @@
 
 package io.github.dasperal
 
+import jetbrains.exodus.entitystore.Entity
 import jetbrains.exodus.entitystore.EntityIterable
 import jetbrains.exodus.entitystore.PersistentEntityStores
+import jetbrains.exodus.entitystore.StoreTransaction
 import jetbrains.exodus.env.Environments.newInstance
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.StringJoiner
 
 object Main
 {
     private val issue_links: MutableList<IssueLink> = ArrayList()
 
     // Arg0 - path to youtrack db
+    // Arg1 - path to export directory
     @JvmStatic
     fun main(args: Array<String>)
     {
         val env = newInstance(args[0])
+        val export_root = Paths.get(args[1])
         val store = PersistentEntityStores.newInstance(env, "teamsysstore")
         store.executeInTransaction {
             find_issue_links(it.getAll("IssueLinkPrototype"))
+            export_issue_links(issues_with_links(it), export_root)
         }
     }
 
@@ -46,6 +55,63 @@ object Main
                 )
             )
         }
+    }
+
+    private fun issues_with_links(txn: StoreTransaction): EntityIterable
+    {
+        var issue_entities =
+            txn.findWithLinks("Issue", issue_links[0].s_to_t_association)
+                .union(txn.findWithLinks("Issue", issue_links[0].t_to_s_association))
+        for(link in issue_links.subList(1, issue_links.size))
+        {
+            issue_entities =
+                issue_entities.union(txn.findWithLinks("Issue", link.s_to_t_association))
+                    .union(txn.findWithLinks("Issue", link.t_to_s_association))
+        }
+        return issue_entities
+    }
+
+    private fun export_issue_links(issue_entities: EntityIterable, export_root: Path)
+    {
+        val csv = StringJoiner("\n")
+        val header = StringJoiner(";").add("id")
+        for(link in issue_links)
+        {
+            header.add(link.s_to_t_name)
+            header.add(link.t_to_s_name)
+        }
+        csv.add(header.toString())
+
+        for(entity in issue_entities)
+        {
+            val line = StringJoiner(";").add(issue_id(entity))
+            for(link in issue_links)
+            {
+                line.add(linked_issue_ids(entity, link.s_to_t_association))
+                line.add(linked_issue_ids(entity, link.t_to_s_association))
+            }
+            csv.add(line.toString())
+        }
+        val links_file_path = export_root.resolve("issue_links.csv")
+        println("Writing issue links to \"${links_file_path.toAbsolutePath()}\"")
+        Files.writeString(links_file_path, csv.toString())
+    }
+
+    private fun linked_issue_ids(entity: Entity, link_name: String): String
+    {
+        val links = StringJoiner(",")
+        for(issue in entity.getLinks(link_name))
+        {
+            links.add(issue_id(issue))
+        }
+        return links.toString()
+    }
+
+    private fun issue_id(entity: Entity): String
+    {
+        val number = entity.getProperty("numberInProject") as Long
+        val project_prefix = entity.getLink("project")?.getProperty("shortName") as String
+        return "$project_prefix-$number"
     }
 
     @JvmRecord
